a8cc0cf2cd407fefa1370ae324b7843a29ecc0c8
eastrand <eemils@gmail.com>
2020-12-01 18:12:07 +0100

    muligfix

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import Home from '../pages/Home';
+import Offices from './../pages/Offices';
 
 const Routes = () => (
     <Router>
