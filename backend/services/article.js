1fb8193e226241ae4ca6fde88f59d21dab11fdac
Snailey <jaran.martinsen@online.no>
2020-12-03 00:07:49 +0100

    populate the articles with author from userScheme

--- backend/services/article.js
+++ backend/services/article.js
@@ -6,13 +6,15 @@ export const createArticle = async (data) => Article.create(data);
 
 // GET
 // get article by ID
-export const getArticleById = async (id) => Article.findById(id);
+export const getArticleById = async (id) =>
+  Article.findById(id).populate('author');
 
 // get list of all articles
-export const listArticles = async () => Article.find();
+export const listArticles = async () => Article.find().populate('author');
 
 // get list of all public articles
-export const listPublicArticles = async () => Article.find({ public: true });
+export const listPublicArticles = async () =>
+  Article.find({ public: true }).populate('author');
 
 // PUT
 // edit article
