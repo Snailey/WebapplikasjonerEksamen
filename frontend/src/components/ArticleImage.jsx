5442c716f75b3223d29eb62ed9c268ac6c077eba
eastrand <eastrand@users.noreply.github.com>
2020-12-12 19:28:58 +0100

    image 100%

--- frontend/src/components/ArticleImage.jsx
+++ frontend/src/components/ArticleImage.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-imagimport { React, useState, useEffect } from 'react';
+import { React, useState, useEffect } from 'react';
 import { get } from '../utils/imageService';
 import {
   ImageContainer,
@@ -19,34 +19,16 @@ const ArticleImage = ({ id }) => {
         setError('fetching image source failed');
       } else {
         console.log(JSON.stringify(data));
-        setSrc(data?.data?.data?.path);
+        let path = data?.data?.data?.path;
+        path = path.substring(path.lastIndexOf('\\') + 1);
+        setSrc(`http://localhost:5000/images/${path}`);
       }
     };
     fetchData();
   }, []);
 
-  /* function arrayBufferToBase64(buffer) {
-    let binary = '';
-    const bytes = [].slice.call(new Uint8Array(buffer));
-    bytes.forEach((b) => (binary += String.fromCharCode(b)));
-    return window.btoa(binary);
-  }
-
-  const downloadImage = async () => {
-    const { data } = await download(id);
-    console.log(data);
-    const img = await data.arrayBuffer().then((buffer) => {
-      const base64Flag = 'data:image/jpeg;base64,';
-      const imageStr = arrayBufferToBase64(buffer);
-      return base64Flag + imageStr;
-    });
-    console.log(img);
-    // const imgUrl = `${process.env.BASE_URL}/${data?.data?.imagePath}`;
-    setSrc(img);
-  }; */
   return (
     <>
-      <p>Test{`thisisthe${src}`}</p>
       {error && <ErrorMessage>{error}</ErrorMessage>}
       {src && (
         <ImageContainer>
